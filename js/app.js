1bde6c6870fb3908fd449a15d65c308499b4ca56
Dustin Brown <dbrown9@gmail.com>
2014-08-06 22:12:11 -0700

    add ability to create a help ticket

--- js/app.js
+++ js/app.js
@@ -1,5 +1,31 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
+  this.route('help');
+});
 
+// Routes
+App.HelpRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('ticket')
+  }
+});
+
+// Controllers
+App.HelpController = Ember.ObjectController.extend({
+  actions: {
+    createTicket: function() {
+      this.get('model').set('open', true).save();
+    }
+  }
+})
+
+// Models
+App.Ticket = DS.Model.extend({
+  student: DS.attr('string'),
+  open: DS.attr('boolean')
+});
+
+App.ApplicationAdapter = DS.FirebaseAdapter.extend({
+  firebase: new Firebase('https://ember-queue.firebaseio.com')
 });
